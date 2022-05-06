5e07bfc38e8d96a087d8b94ee2d8dbca40e02e10
deanmooore <deanpmoore@gmail.com>
2022-05-05 18:41:02 -0700

    lightswitch getting somewhere at least

--- lightswitch.py
+++ lightswitch.py
@@ -1,7 +1,22 @@
 #main file for GUI
+from phue import Bridge 
+import tkinter as tk
+
+b = Bridge('10.0.0.61')
 
 def main():
+    w = tk.Tk()
+    w.geometry = ("610x400")
  
+    def set_lights():
+        for l in b.lights:
+            l.on = True
+            l.brightness=dimmer.get()
+    
+    dimmer = tk.Scale(w, variable=254, sliderlength = 50, width = 50, length = 380,command=set_lights)
+    dimmer.pack()
+    w.mainloop()
 
-if "__name__" == __main__:
+
+if __name__ =="__main__":
  main()
