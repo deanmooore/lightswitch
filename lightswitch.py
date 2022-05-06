5257aa555e492ca7b185fa5af82c876ca0e933c2
deanmooore <deanpmoore@gmail.com>
2022-05-05 22:19:56 -0700

    lights change now

--- lightswitch.py
+++ lightswitch.py
@@ -7,13 +7,16 @@ b = Bridge('10.0.0.61')
 def main():
     w = tk.Tk()
     w.geometry = ("610x400")
- 
-    def set_lights():
-        for l in b.lights:
-            l.on = True
-            l.brightness=dimmer.get()
     
-    dimmer = tk.Scale(w, variable=254, sliderlength = 50, width = 50, length = 380,command=set_lights)
+    lights = b.lights
+
+    def set_lights(bri):
+        for l in lights:
+            print(bri)
+            l.on = True
+            l.brightness=int(bri)
+    var = tk.DoubleVar()    
+    dimmer = tk.Scale(w, from_ = 0, to = 254, variable=var, sliderlength = 50, width = 50, length = 380,command=set_lights)
     dimmer.pack()
     w.mainloop()
 
